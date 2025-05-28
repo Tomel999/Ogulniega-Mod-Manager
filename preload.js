14ac304e567aa033c2e41c895b0b4575e7d7ce52
tomel999 <101830048+Tomel999@users.noreply.github.com>
2025-05-28 21:28:50 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,19 +1,22 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  downloadFile: (args) => ipcRenderer.invoke('download-file', args),
-  showErrorMessage: (args) => ipcRenderer.send('show-error-message', args),
-  showInfoMessage: (args) => ipcRenderer.send('show-info-message', args),
-  getProfileFolders: () => ipcRenderer.invoke('get-profile-folders'),
-  browseForDirectory: () => ipcRenderer.invoke('browse-for-directory'),
-  onDownloadStarted: (callback) => ipcRenderer.on('download-started', (_event, value) => callback(value)),
-  onDownloadProgress: (callback) => ipcRenderer.on('download-progress', (_event, value) => callback(value)),
-  onDownloadComplete: (callback) => ipcRenderer.on('download-complete', (_event, value) => callback(value)),
-  onDownloadError: (callback) => ipcRenderer.on('download-error', (_event, value) => callback(value)),
-  removeAllDownloadListeners: () => {
-    ipcRenderer.removeAllListeners('download-started');
-    ipcRenderer.removeAllListeners('download-progress');
-    ipcRenderer.removeAllListeners('download-complete');
-    ipcRenderer.removeAllListeners('download-error');
-  }
+    getProfileFolders: () => ipcRenderer.invoke('get-profile-folders'),
+    browseForDirectory: () => ipcRenderer.invoke('browse-for-directory'),
+    downloadFile: (args) => ipcRenderer.invoke('download-file', args),
+    getPreinstalledMods: (profilePath) => ipcRenderer.invoke('get-preinstalled-mods', profilePath),
+    showItemInFolder: (filePath) => ipcRenderer.send('show-item-in-folder', filePath),
+    toggleModState: (currentPath) => ipcRenderer.invoke('toggle-mod-state', currentPath),
+    showErrorMessage: (args) => ipcRenderer.send('show-error-message', args),
+    showInfoMessage: (args) => ipcRenderer.send('show-info-message', args),
+    onDownloadStarted: (callback) => ipcRenderer.on('download-started', (_event, value) => callback(value)),
+    onDownloadProgress: (callback) => ipcRenderer.on('download-progress', (_event, value) => callback(value)),
+    onDownloadComplete: (callback) => ipcRenderer.on('download-complete', (_event, value) => callback(value)),
+    onDownloadError: (callback) => ipcRenderer.on('download-error', (_event, value) => callback(value)),
+    removeAllDownloadListeners: () => {
+        ipcRenderer.removeAllListeners('download-started');
+        ipcRenderer.removeAllListeners('download-progress');
+        ipcRenderer.removeAllListeners('download-complete');
+        ipcRenderer.removeAllListeners('download-error');
+    }
 });
